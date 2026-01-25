Enhance classification training and metrics logging - cross validation for the readout training - correct implementation of kernel rank on the whole dataset - separate rank computation making two different functions for trajectories and data matrices

att_dim_experiments/classification.py
@@ -4,12 +4,13 @@ import numpy as np
 import wandb
 from typing import Optional
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import RidgeClassifierCV
 from acds.archetypes import InterconnectionRON
 from acds.networks import ArchetipesNetwork, random_matrix, full_matrix, cycle_matrix, deep_reservoir, star_matrix, local_connections
 from aeon.datasets import load_classification
 from acds.networks.utils import unstack_state
 from att_dim_experiments.metrics import *
+from att_dim_experiments.lyap_discreteRNN import compute_lyapunov
 
 def get_connection_matrix(name: str, n_modules: int, p: float = 0.5, seed: Optional[int] = None) -> torch.Tensor:
     cm_dict = {"random": random_matrix, 
@@ -74,8 +75,8 @@ def get_data(args):
 
     test_dataset, test_target = load_classification(name=args['dataset'], split="test", extract_path='att_dim_experiments/data')
     test_dataset = np.permute_dims(test_dataset, [0, 2, 1])
-    train_dataset, val_dataset, train_target, val_target = train_test_split(train_dataset, train_target, random_state=42, test_size=0.25, stratify=train_target)
-    return (train_dataset, train_target.astype(int)), (val_dataset, val_target.astype(int)), (test_dataset, test_target.astype(int))
+   # train_dataset, val_dataset, train_target, val_target = train_test_split(train_dataset, train_target, random_state=42, test_size=0.25, stratify=train_target)
+    return (train_dataset, train_target.astype(int)), (test_dataset, test_target.astype(int))
 
 
 def get_params_from_model(model: ArchetipesNetwork):
@@ -94,12 +95,12 @@ def train_readout(states:torch.Tensor, labels:torch.Tensor, ridge_alpha:float):
     n_modules, n_hid = states.shape[1], states.shape[-1]
     X = states[-1, :, :, 0, :].reshape(states.shape[2], n_modules * n_hid).numpy()  # (len_seq, n_mod, n_samples, 2, h_dim) -> (n_samples, n_mod * h_dim)
     y = labels
-    ridge = RidgeClassifier(alpha=ridge_alpha)
+    ridge = RidgeClassifierCV(cv=10)
     ridge.fit(X, y)
     return ridge
 
 
-def evaluate(states:torch.Tensor, labels:torch.Tensor, readout:RidgeClassifier):
+def evaluate(states:torch.Tensor, labels:torch.Tensor, readout:RidgeClassifierCV):
     n_modules, n_hid = states.shape[1], states.shape[-1]
     X = states[-1, :, :, 0, :].reshape(states.shape[2], n_modules * n_hid).numpy()  # (len_seq, n_mod, n_samples, 2, h_dim) -> (n_samples, n_mod * h_dim)
     y = labels.numpy()
@@ -110,41 +111,83 @@ def run_classification():
     args=parse_args()
     args_dict = vars(args)
 
-    wandb.init(project=f"archetype-classification", config=args_dict)
+    with wandb.init(project=f"archetype-classification", config=args_dict):
 
-    (train_data, train_labels), (val_data, val_labels), (test_data, test_labels) = get_data(args_dict)
+        (train_data, train_labels), (test_data, test_labels) = get_data(args_dict)
 
-    if not args.silent:
-        print(f"Train data shape: {train_data.shape}, Train labels shape: {train_labels.shape}")
-        print(f"Validation data shape: {val_data.shape}, Validation labels shape: {val_labels.shape}")
-        print(f"Test data shape: {test_data.shape}, Test labels shape: {test_labels.shape}")
+        if not args.silent:
+            print(f"Train data shape: {train_data.shape}, Train labels shape: {train_labels.shape}")
+            print(f"Test data shape: {test_data.shape}, Test labels shape: {test_labels.shape}")
 
-    # Get model
-    n_input = train_data.shape[-1] if len(train_data.shape) > 1 else 1
-    model = get_model(args_dict, n_input=n_input)
-    params = get_params_from_model(model)
-    if not args.silent:
-        print(f"Model architecture: {model}")
-    
-        # Get reservoir states
-    train_states, train_fbs = compute_states(model, torch.Tensor(train_data)) # (seq_len, n_modules, n_states, n_hid)
-    val_states, val_fbs = compute_states(model, torch.Tensor(val_data))
-    test_states, test_fbs = compute_states(model, torch.Tensor(test_data))
-    if not args.silent:
-        print(f"Train states shape: {train_states.shape}")
-        print(f"Val states shape: {val_states.shape}")
-        print(f"Test states shape: {test_states.shape}")
+        # Get model
+        n_input = train_data.shape[-1] if len(train_data.shape) > 1 else 1
+        model = get_model(args_dict, n_input=n_input)
+        params = get_params_from_model(model)
+        if not args.silent:
+            print(f"Model architecture: {model}")
+        
+            # Get reservoir states
+        train_states, train_fbs = compute_states(model, torch.Tensor(train_data)) # (seq_len, n_modules, batch_size n_states, n_hid)
+        test_states, test_fbs = compute_states(model, torch.Tensor(test_data))
+        if not args.silent:
+            print(f"Train states shape: {train_states.shape}")
 
-    readout = train_readout(train_states, torch.Tensor(train_labels), ridge_alpha=args.ridge_alpha)
-    if not args.silent:
-        print("Readout trained.")
-    # Evaluate on validation and test sets
-    train_score = evaluate(train_states, torch.Tensor(train_labels), readout)
-    val_score = evaluate(val_states, torch.Tensor(val_labels), readout)
-    test_score = evaluate(test_states, torch.Tensor(test_labels), readout)
+            print(f"Test states shape: {test_states.shape}")
+
+        readout = train_readout(train_states, torch.Tensor(train_labels), ridge_alpha=args.ridge_alpha)
+        if not args.silent:
+            print("Readout trained.")
+        # Evaluate on validation and test sets
+        train_score = evaluate(train_states, torch.Tensor(train_labels), readout)
+
+        test_score = evaluate(test_states, torch.Tensor(test_labels), readout)
+
+
+        train_states_np = np.transpose(train_states.numpy(), [2, 0, 1, 3, 4]) # (bs, seq_len, n_modules, 2, n_hid)
+        train_fbs_np = np.transpose(train_fbs.numpy(), [2, 0, 1, 3])
+        
+        corr_dims = []
+        part_ratios = []
+        lyapunov_exps = []
+        for state, fb in zip(train_states_np, train_fbs_np):
+            state = state[:, :, 0]
+            corr_dims.append(compute_corr_dim(state, transient=0))
+            part_ratios.append(compute_participation_ratio(state, transient=0))
+            l = []
+            for i, p in enumerate(params):
+                l.append(compute_lyapunov(nl=2, W=p['h2h']. numpy().T, V=p['x2h'].numpy().T, b=p['bias'].numpy(), h_traj=state[:, i], u_traj=train_data[i], fb_traj=fb[:, i]))
+            lyapunov_exps.append(l)
+        mles = np.mean(np.array(lyapunov_exps), axis=0)[:, 0]
+        eff_ranks = compute_effective_kernel_rank(train_states_np[:, :, :, 0])
+
+
+        metrics = {
+            "train_acc": train_score,
+            "val_acc": readout.best_score_,
+            "test_acc": test_score,
+            "att_dim/correlation_dimension_mean": np.mean(corr_dims),
+            "att_dim/participation_ratio_mean": np.mean(part_ratios),
+            "att_dim/effective_rank_mean": np.mean(eff_ranks),
+            "lyapunov/max_lyap_exp": mles,
+            "lyapunov/mle_mean": mles.mean()
+        }
+        metrics.update(
+        {"att_dim/correlation_dimension": corr_dims,
+        "att_dim/participation_ratio": part_ratios,
+        "att_dim/effective_rank": eff_ranks,
+        })
+
+
+        wandb.log(metrics)
+
+
+
+
+
+
+
     if not args.silent:
         print(f"Training score: {train_score}")
-        print(f"Validation score: {val_score}")
         print(f"Test score: {test_score}")
 
 

att_dim_experiments/metrics.py
@@ -59,7 +59,7 @@ def compute_participation_ratio(trajectory, transient=4000) -> list[float]:
     return participation_ratios
 
 
-def compute_effective_rank(trajectory, transient=4000, eps = 1e-10) -> list[float]:
+def compute_effective_traj_rank(trajectory, transient=4000, eps = 1e-10) -> list[float]:
     """
     Compute Effective Rank for each module in a set of trajectories
     Args:
@@ -78,6 +78,29 @@ def compute_effective_rank(trajectory, transient=4000, eps = 1e-10) -> list[floa
         entropy = - np.dot(n_singvals + eps, np.log(n_singvals + eps)) 
         ranks.append(np.exp(entropy))
     return ranks
+
+
+def compute_effective_kernel_rank(trajectory, eps = 1e-10) -> list[float]:
+    """
+    Compute Effective Rank for each module in a set of training states
+    Args:
+        trajectory (np.ndarray): trajectory of shape (batch_size, N_steps, N_modules, N_h)
+    Returns:
+        Optional[list[float]]: List of effective rank estimates for each module, or None if an error occurs
+    """
+    n_modules = trajectory.shape[2]
+    ranks = []
+    for i in range(n_modules):
+        kernel_j = trajectory[:, -1, i]
+
+        singvals = np.linalg.svdvals(kernel_j)
+        s = max(np.sum(np.abs(singvals)), eps)
+        p = singvals / s
+        p = p[p > 0]
+        entropy =  -np.sum(p * np.log(p + eps))
+        ranks.append(np.exp(entropy))
+    return ranks
+
 
 
 def nrmse(preds: np.ndarray, target: np.ndarray) -> float:
@@ -156,7 +179,7 @@ def compute_lyapunov(model:ArchetipesNetwork, trajectory: np.ndarray, inputs: np
     return exponents
 
 
-__all__ = ['compute_corr_dim', 'compute_participation_ratio', 'compute_effective_rank', 'nrmse']
+__all__ = ['compute_corr_dim', 'compute_participation_ratio', 'compute_effective_traj_rank', 'nrmse', 'compute_effective_kernel_rank']
 
 if __name__ == '__main__':
     # simple test
@@ -173,6 +196,6 @@ if __name__ == '__main__':
     fbs = fbs.detach().numpy()
     print("Correlation Dimension:", compute_corr_dim(traj))
     print("Participation Ratio:", compute_participation_ratio(traj))
-    print("Effective Rank:", compute_effective_rank(traj))
+    print("Effective Rank:", compute_effective_traj_rank(traj))
     print("Lyapunov Exponents:", compute_lyapunov(model, traj, inputs, fbs, n_lyap=1))
 