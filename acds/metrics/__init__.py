02848712c2e637f1b51ce0a7a26dde68299fae24
g-lago8 <lagomarsinigiacomo@gmail.com>
2026-03-17 14:00:29 +0000

    Add __init__.py to the metrics module

---
+++ acds/metrics/__init__.py
@@ -0,0 +1,2 @@
+from .metrics import compute_corr_dim, compute_participation_ratio, compute_effective_rank, nrmse
+from lyap_discreteRNN import compute_lyapunov # Lyapunov exponent computation for ESN archetype
